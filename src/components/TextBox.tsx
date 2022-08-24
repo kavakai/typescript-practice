667b37ae2e7fd2477428078f822687f574282a8f
Kai Kuller <kavakai@gmail.com>
2022-08-24 15:33:45 -0600

    Add styling to input box

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import "./styles.css"
 
 const TextBox: React.FC = () => {
   return (
     <div>
-      <input type="text"></input>
+      <form className="input">
+        <input type="input" placeholder="Enter Task Here" className="input_box"></input>
+        <button className="input_submit" type="submit">Go</button>
+      </form>
     </div>
   )
 }
