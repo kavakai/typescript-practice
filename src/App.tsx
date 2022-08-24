04a20a608d99f85ba2f3f7697b7782410a5d4be9
Kai Kuller <kavakai@gmail.com>
2022-08-24 15:53:43 -0600

    Add todos model interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
 import TextBox from './components/TextBox';
+import { Todo } from './TodoModel';
 
 const App: React.FC = () => {
 
   const[todo, setTodo] = useState<string>("")
-
+  const[todos, setTodos] = useState<Todo[]>([])
 
   return (
     <div className="App">
--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -7,11 +7,18 @@ interface Props {
 }
 
 
-const TextBox = ({ todo, setTodo }: Props) => {
+const TextBox:React.FC<Props>= ({ todo, setTodo }) => {
   return (
     <div>
       <form className="input">
-        <input type="input" placeholder="Enter Task Here" className="input_box"></input>
+        <input 
+          type="input" 
+          placeholder="Enter Task Here" 
+          className="input_box"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        >
+        </input>
         <button className="input_submit" type="submit">Go</button>
       </form>
     </div>
