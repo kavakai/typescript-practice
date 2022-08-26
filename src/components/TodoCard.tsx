0332788f7a31e55586b5be9f21b1c17db3d17ff8
Kai Kuller <kavakai@gmail.com>
2022-08-26 11:08:39 -0600

    Add done method to card

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -11,18 +11,39 @@ interface Props {
 }
 
 const TodoCard: React.FC<Props> = ({ todo, todos, setTodos }) => {
+
+  const handleDone = (id: number) => {
+    setTodos(todos.map(todo => {
+      return todo.id === id ? { ...todo, isDone:!todo.isDone } : todo
+    }))
+  }
+
+  const handleDelete = (id: number) => {
+
+  }
+
+  const handleEdit = (id: number) => {
+
+  }
+
   return (
     <form className="single_todo">
-      <span className="single_todos--text">
-        {todo.todo}
-      </span>
-      <span className="icon">
+      { todo.isDone ? 
+        (<s className="single_todos--text">
+         {todo.todo}
+        </s> ): 
+        (<span className="single_todos--text">
+          {todo.todo}
+         </span> )
+      }
+      
+      <span className="icon" onClick={() => handleEdit(todo.id)}>
         <AiFillEdit />
       </span>
-      <span className="icon">
+      <span className="icon" onClick={() => handleDelete(todo.id)}>
         <AiFillDelete />
       </span>
-      <span className="icon">
+      <span className="icon" onClick={() => handleDone(todo.id)}>
         <MdDone />
       </span>
     </form>
